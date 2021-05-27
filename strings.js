1097947c7a4660a0b9a7b5e48b6ce4315aebed62
Andy <andrew.mcginn@gmail.com>
2021-05-27 21:56:34 +0100

    string to hash kata added

--- strings.js
+++ strings.js
@@ -17,3 +17,16 @@ decodeMorse = function(morseCode){
   return humanWordArray.join(' ');
 }
 
+//Turn String Input into Hash - user4655293
+//"a=1, b=2, c=3, d=4" => { 'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+function strToHash(str){
+  const res = {};
+  if (str) {
+    str.split(', ').map(pair => {
+      const [key, val] = pair.split('=');
+      res[key] = Number(val);
+    });
+  }
+  return res; 
+}
