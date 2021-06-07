bbf71c208f28b85f77a851570d9f46a31dc741dd
Andy <andrew.mcginn@gmail.com>
2021-06-07 17:24:29 +0100

    unfinished mutation game added

--- strings.js
+++ strings.js
@@ -30,3 +30,39 @@ function strToHash(str){
   }
   return res; 
 }
+
+
+// unfinished - mutations game 
+function mutations(alice, bob, startingWord, first){
+  //game setup
+  let currentPlayer = first === 0 ? alice : bob;
+  let currentPlayerName = first === 0 'alice' : 'bob';
+  let currentWord = startingWord;
+  // take a turn
+  let i = 0
+  while (currentWord != undefined) {
+      currentWord = takeTurn(currentWord, currentPlayer)[0]
+      console.log({currentPlayer}, {currentWord})
+      i++;
+  }
+} 
+
+function takeTurn(lastWord, playerWords) {
+  let newWord = playerWords.filter(word => {
+    let sameChars = 0;
+   [...lastWord].forEach(char => {
+      if (word.includes(char)) {
+        sameChars++;
+      }
+    })
+    return sameChars === 3;
+     })
+  // remove word from memory
+  playerWords.splice(playerWords.indexOf(newWord[0]), 1)
+  return newWord
+}
+
+const alice = ['plat', 'rend', 'bear', 'soar', 'mare', 'pare', 'flap', 'neat', 'clan', 'pore'], bob = ['boar', 'clap', 'farm', 'lend', 'near', 'peat', 'pure', 'more', 'plan', 'soap'];
+
+
+mutations(alice,bob,'maze',0)
